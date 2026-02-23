ac74cb8320b258f679e39b62e9edc9706bb6ffe4
barakael <barakaellucas2019@gmail.com>
2026-02-23 11:06:12 +0300

    addind CRUD functionalities in the Policy page

--- Insurance_system/models/policy.py
+++ Insurance_system/models/policy.py
@@ -1,58 +1,30 @@
+from sqlalchemy import Column, Integer, String, Float
+from database.session import Base
 from abc import ABC, abstractmethod
 
-class Policy(ABC):
+# 1. The Abstract Base Class (Logic)
+class Policy(Base):
+    __tablename__ = "policies"
     
-    def __init__(self, policy_id, policy_type, start_date, end_date):
-        # Encapsulation (private attributes)
-        self.__policy_id = policy_id
-        self.__policy_type = policy_type
-        self.__start_date = start_date
-        self.__end_date = end_date
-        self.__active = False
-        self.__premiums = []
-        self.__claims = []
+    id = Column(Integer, primary_key=True, index=True)
+    holder = Column(String)
+    type = Column(String) # 'Auto', 'Life', etc.
+    premium = Column(Float)
+    status = Column(String, default="Active")
 
-
-
-    # Getter methods
-    def get_policy_id(self):
-        return self.__policy_id
-
-    def get_policy_type(self):
-        return self.__policy_type
-
-    def get_start_date(self):
-        return self.__start_date
-
-    def get_end_date(self):
-        return self.__end_date
-
-    def is_active(self):
-        return self.__active
-
-    # Business methods
-    def activate_policy(self):
-        self.__active = True
-        print("Policy activated successfully.")
-
-    def cancel_policy(self):
-        self.__active = False
-        print("Policy cancelled successfully.")
-        
-    def add_premium(self, premium):
-        self.__premiums.append(premium)
-
-    def get_premiums(self):
-        return self.__premiums
-    def add_claim(self, claim):
-        self.__claims.append(claim)
-
-    def get_claims(self):
-        return self.__claims
-
-
-
-    # Abstract method (Polymorphism will happen here)
+    # This makes the class Abstract in OOP
     @abstractmethod
     def calculate_premium(self):
         pass
+
+# 2. The Concrete Classes (Implementation)
+# These are what actually get saved to the DB
+class AutoPolicy(Policy):
+    def calculate_premium(self):
+        # Example logic: Auto policies have a base tax
+        return self.premium * 1.1
+
+class LifePolicy(Policy):
+    def calculate_premium(self):
+        # Example logic: Life policies have different risk rates
+        return self.premium * 1.2
