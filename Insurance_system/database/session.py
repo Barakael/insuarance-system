2f38fdc26e1d817ed20dc33f8b40dddedbeda3b4
barakael <barakaellucas2019@gmail.com>
2026-02-23 09:01:29 +0300

    database changes and connection

--- Insurance_system/database/session.py
+++ Insurance_system/database/session.py
@@ -1,11 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# If root has NO password
-DATABASE_URL = "mysql+pymysql://root:kelvin.2908@localhost/insurance_db"
-
-# If you have password:
-# DATABASE_URL = "mysql+pymysql://root:YOUR_PASSWORD@localhost/insurance_db"
+# DATABASE_URL = "mysql+pymysql://root:kelvin.2908@localhost/insurance_db"
+DATABASE_URL = "sqlite:///insurance.db"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
